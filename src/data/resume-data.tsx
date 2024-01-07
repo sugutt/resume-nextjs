cbe7dec8e004dc4ad4e5ae2eee0c18382cde1f47
Sugutt <97742262+sugutt@users.noreply.github.com>
2024-01-07 21:35:35 +0300

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -44,7 +44,7 @@ export const RESUME_DATA = {
   ],
   work: [
     {
-      company: "567",
+      company: "789",
       link: "https://jxnl.notion.site/jxnl/whats-up-95708748a05d40c398a4abc66a978a8e",
       badges: ["Consulting"],
       title: "Owner / Consultant",
@@ -55,9 +55,9 @@ export const RESUME_DATA = {
         "Working with seed stage companies, helping founders with tech strategy, research, and infrastructure.",
     },
     {
-      company: "South Park Commons",
-      link: "https://www.southparkcommons.com/",
-      badges: ["Sabbatical"],
+      company: "Student Representative Computing",
+      link: "www.riarauniversity.co.ke",
+      badges: ["Leadership"],
       title: "Member",
       logo: "", // Add logo if available
       start: "2023",
@@ -66,61 +66,30 @@ export const RESUME_DATA = {
         "Exploring LLM tooling around structured extraction and self-hosting. Launched Youtube University and developing Journal, an AI-powered journaling application.",
     },
     {
-      company: "Stitchfix",
-      link: "https://www.stitchfix.com/",
-      badges: ["System"],
-      title: "Staff ML Engineer",
+      company: "HashNode",
+      link: "https://sugutt.hashnode.dev/",
+      badges: ["Writer"],
+      title: "Technical Writing",
       logo: "", // Add logo if available
-      start: "2018",
+      start: "2022",
       end: "2023",
       description: (
         <>
           <li>
-            Designed Flight framework, improving reliability, scalability,
-            adopted by over 80% of organization, servicing 350 million daily
-            requests.{" "}
-            <a href="https://newsroom.stitchfix.com/blog/your-shop-is-getting-an-update-shop-by-category-will-make-it-easier-to-discover-things-youll-love/">[1]</a>
-          </li>
-          <li>
-            Led vision research, managed data acquisition, fine-tuned models, developed
-            300,000+ outfit dataset to power new Stitch Fix outfit recommender. 
-          </li>
-          <li>
-            Developed product similarity search with multi-modal embeddings,
-            boosting revenue by over $50 million annually. <a href="https://newsroom.stitchfix.com/blog/how-we-understand-your-personal-style/">[2]</a>
-          </li>
+            Wrote articles on machine learning to improve how to better 
+            present and communicate my projects.
+           </li>
         </>
       ),
     },
-    {
-      company: "Facebook",
-      link: "https://about.meta.com/actions/promoting-safety-and-expression/",
-      badges: ["Data Science"],
-      title: "Data Scientist",
-      logo: "",
-      start: "2017",
-      end: "2017",
-      description:
-        "Created algorithms for identifying harmful content and developed dashboards to detect malicious activities.",
-    },
-    {
-      company: "New York University",
-      link: "https://www.nyu.edu/",
-      badges: ["Research"],
-      title: "Research Assistant",
-      start: "2015",
-      end: "2017",
-      description:
-        "Under Rumi Chunara, centered around public health and social media.",
-      logo: "", // Path to NYU logo image
-    },
+   
   ],
   skills: [
     "Python",
     "Pytorch",
     "LLMs",
     "Prompt Engineering",
-    "Structured Extraction",
+    "Computer Vision",
     "Observability",
     "Developer Tools",
     "Machine Learning",
@@ -165,101 +134,6 @@ export const RESUME_DATA = {
       link: {
         label: "Journal",
         href: "http://usejournal.xyz",
-      },
-    },
-  ],
-  clients: [
-    {
-      title: "Rewind AI",
-      techStack: ["memory", "personal computing"],
-      description: "Rewind AI is a personal memory assistant that helps you remember, organize, and navigate your life.",
-      link: {
-        label: "rewind.ai",
-        href: "http://rewind.ai",
-      },
-    },
-    {
-      title: "Naro",
-      techStack: ["python", "prompt engineering", "sales"],
-      description:
-        "Naro sits in the background, using contextual clues to proactively select the right content and messaging for each customer conversation, from emails to meetings.",
-      link: {
-        label: "narohq.com",
-        href: "http://narohq.com",
-      },
-    },
-    {
-      title: "Trunk Tools",
-      techStack: [
-        "construction",
-        "technical advisor",
-        "retrival",
-      ],
-      description:
-        "Trunk Tools addresses the skilled labor shortage in construction, enhancing workforce productivity through AI-based tools.",
-      link: {
-        label: "trunktools.com",
-        href: "https://trunktools.com/",
-      },
-    },
-    {
-      title: "Modal Labs",
-      techStack: ["technical writing", "python"],
-      description:
-        "Modal specializes in cloud functions, offering a platform for running generative AI models, large-scale batch jobs, and more.",
-      link: {
-        label: "modal.com",
-        href: "https://modal.com/",
-      },
-    },
-    {
-      title: "Pydantic",
-      techStack: ["technical writing", "python"],
-      description:
-        "Pydantic provides data validation and settings management using Python type annotations, enforcing type hints at runtime with user-friendly error handling.",
-      link: {
-        label: "pydantic.dev",
-        href: "http://pydantic.dev",
-      },
-    },
-    {
-      title: "Weight & Biases",
-      techStack: [
-        "technical writing",
-        "education",
-        "machine learning",
-      ],
-      description:
-        "Wandb (Weights & Biases) provides a platform for tracking machine learning experiments, offering tools for visualization, comparison, and collaboration in ML projects.",
-      link: {
-        label: "wandb.ai",
-        href: "https://wandb.ai/",
-      },
-    },
-    {
-      title: "New Computer",
-      techStack: ["personal computing", "python"],
-      description:
-        "Dot by New Computer is an intelligent guide designed to help you remember, organize, and navigate your life.",
-      link: {
-        label: "new.computer",
-        href: "http://new.computer",
-      },
-    },
-
-    {
-      title: "Kay.ai",
-      techStack: [
-        "retrieval",
-        "evals",
-        "infrastucture",
-        "python",
-      ],
-      description:
-        "Retrieve relevant context from the semantic web for your LLM apps with fully hosted embeddings.",
-      link: {
-        label: "Kay.ai",
-        href: "http://Kay.ai",
       },
     },
   ],
